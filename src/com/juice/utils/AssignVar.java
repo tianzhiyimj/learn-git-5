2a3bc665f79beaed569c023c3570fc69125308a8
10672661 <10672661@qq.com>
2020-04-21 13:42:59 +0800

    add annotation and change variable
    Signed-off-by: 10672661 <10672661@qq.com>

--- src/com/juice/utils/AssignVar.java
+++ src/com/juice/utils/AssignVar.java
@@ -1,14 +1,17 @@
 package com.juice.utils;
 
+/**
+ * 为参数赋值
+ */
 public class AssignVar {
     /**
- * 将输入的字符串分割
- * 调用Transfer方法转换str[3]，为整型
- */
+     * 将输入的字符串分割，调用Transfer方法转换str[3]，为整型
+     * @param str 传入的字符串
+     */
     public static void assignVar(String str){
         String[] str1=str.split("\\s+");
-        int Toint=Transfer.toNum(str1[3]);
-        System.out.println(Toint);
-        Variable.setVar(str1[1],Toint);
+        int toInt=Transfer.toNum(str1[3]);
+        System.out.println(toInt);
+        Variable.setVar(str1[1],toInt);
     }
 }
