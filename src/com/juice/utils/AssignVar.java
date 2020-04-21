dbbe8db4a149c08bc18f83b1c4236f8ef91c7072
10672661 <60462210+10672661@users.noreply.github.com>
2020-04-21 13:24:00 +0800

    add annotation

--- src/com/juice/utils/AssignVar.java
+++ src/com/juice/utils/AssignVar.java
@@ -1,6 +1,10 @@
 package com.juice.utils;
 
 public class AssignVar {
+    /**
+ * 将输入的字符串分割
+ * 调用Transfer方法转换str[3]，为整型
+ */
     public static void assignVar(String str){
         String[] str1=str.split("\\s+");
         int Toint=Transfer.toNum(str1[3]);
