419e6919861dcfa174a578c2b675a50f67998d1d
cjw0816 <60734880+cjw0816@users.noreply.github.com>
2020-04-21 23:17:45 +0800

    Update 注释

--- src/com/juice/utils/ManipulateNum.java
+++ src/com/juice/utils/ManipulateNum.java
@@ -1,7 +1,16 @@
 package com.juice.utils;
 
+/**
+ * 对变量支持加减法
+ */
 public class ManipulateNum {
     public static void manipulateNum(String str){
+        /**
+         * 读取第二个字符串数组的字符串 判断做加法或减法
+         *
+         * @param str 输入的语句
+         * @param info str拆分后的字符串数组
+         */
         String[] info = str.split("\\s+");
         if(info[1].equals("减少")){
             Variable.setVar(info[0],Variable.getVar(info[0])-Transfer.toNum(info[2]));
