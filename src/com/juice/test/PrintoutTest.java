ca9c5b9061f3db59c5cd796401513584ec00fa54
Mangnolia <tianzhiyimj@163.com>
2020-04-23 23:12:25 +0800

    update Printout.java

--- src/com/juice/test/PrintoutTest.java
+++ src/com/juice/test/PrintoutTest.java
@@ -10,10 +10,10 @@ import static org.junit.Assert.*;
 public class PrintoutTest {
     @Test
     public void PrintoutTest() {
-        String result = Printout.printout(str:"看看 ”我穷死了“");
-        Assert.assertEquals(expected:"我穷死了",result);
-        Variable.setVar(varStr:"气温",var:10);
-        result = Printout.printout(str:"看看 气温");
-        Assert.assertEquals(expected:"10",result);
+        String result = Printout.printout(str"看看 ”我穷死了“");
+        Assert.assertEquals(expected"我穷死了",result);
+        Variable.setVar(varStr"气温",var:10);
+        result = Printout.printout(str"看看 气温");
+        Assert.assertEquals(expected"10",result);
     }
 }
