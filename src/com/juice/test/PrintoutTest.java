904ec642204fad651f2bc78b0bfa6c6c61d3b75a
Mangnolia <tianzhiyimj@163.com>
2020-04-23 11:07:07 +0800

    update Printout.java

---
+++ src/com/juice/test/PrintoutTest.java
@@ -0,0 +1,19 @@
+package com.juice.test;
+
+import com.juice.utils.Variable;
+import com.juice.utils.Printout;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PrintoutTest {
+        @Test
+        public void PrintoutTest() {
+            String result = Printout.printout(str:"看看 ”我穷死了“");
+            Assert.assertEquals(expected:"我穷死了",result);
+            Variable.setVar(varStr:"气温",var:10);
+            result = Printout.printout(str:"看看 气温");
+            Assert.assertEquals(expected:"10",result);
+        }
+    }
---
+++ src/com/juice/utils/Printout.java
@@ -0,0 +1,25 @@
+package com.juice.utils;
+
+import com.juice.utils.Variable;
+/**
+ * 输出字符串或者变量
+ * @param array
+ */
+public class Printout{
+    public static String printOut(String str) {
+        String[] split = str.split(regax:"\\s+");
+        String varStr=split[1];
+        if(varStr.contains("“")&&varStr.contains("“")){
+            String trim = varStr.trim().replace(target:"“",replacement:"").replace(target:"”",replacement:"").trim();
+            System.out.println(trim);
+        }
+        System.out.println(varStr);
+        if (varStr.contains("")){
+            return null ;
+        }
+        else{
+            int var = Variable.getVar(varStr.trim());
+            return ""+var;
+        }
+    }
+}
