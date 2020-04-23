79a1474a331753f8cde69ede3eeabafa44a9280f
Mangnolia <tianzhiyimj@163.com>
2020-04-23 23:30:16 +0800

    update Printout.java

--- src/com/juice/utils/Printout.java
+++ src/com/juice/utils/Printout.java
@@ -3,17 +3,16 @@ package com.juice.utils;
 import com.juice.utils.Variable;
 /**
  * 输出字符串或者变量
- * @param array
+ * @param str 传入字符串 看看 变量 或 看看 字符串
  */
 public class Printout{
     public static String printOut(String str) {
-        String[] split = str.split(regax"\\s+");
+        String[] split = str.split("\\s+");
         String varStr=split[1];
-        if(varStr.contains("“")&&varStr.contains("“")){
-            String trim = varStr.trim().replace(target"“",replacement"").replace(target"”",replacement"").trim();
+        if(varStr.contains("“")&&varStr.contains("”")){
+            String trim = varStr.trim().replace("“","").replace("”","").trim();
             System.out.println(trim);
         }
-        System.out.println(varStr);
         if (varStr.contains("")){
             return null ;
         }
